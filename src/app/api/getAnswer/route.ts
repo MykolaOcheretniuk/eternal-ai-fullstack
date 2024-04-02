564e511347ff6966dae5842581b04058bee55286
Mykola <95630245+miowff@users.noreply.github.com>
2024-04-02 18:31:55 +0300

    Fix: get answer env

--- src/app/api/getAnswer/route.ts
+++ src/app/api/getAnswer/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
     const { message, famousPersonName } = (await request.json()) as RequestBody;
     const ipToken = jwt.sign(
       { ipV4: ipData.query, userAgent: request.headers.get("user-agent") },
-      getEnv("UNAUTHORIZED_USER_SECRET"),
+      process.env.UNAUTHORIZED_USER_SECRET as string,
       { expiresIn: "1h" }
     );
     const answerResponse = await fetch(`${BASE_URL}/message`, {
